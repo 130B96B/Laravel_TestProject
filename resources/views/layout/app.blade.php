1d0d9593d24d5c179c7b32e203506c9c3cc01d21
kite <kaitokitaguchi170@gmail.com>
2024-02-04 20:03:00 +0900

    修正

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -41,7 +41,7 @@
         <p>
             <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                 @csrf
-                <a class="dropdown-item"
+                <a class="dropdown-item"  href=""
                     onclick="event.preventDefault();
                              document.getElementById('logout-form').submit();">
                     {{ __('ログアウト') }}
